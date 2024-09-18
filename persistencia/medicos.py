16e921fac3f376c763f3287e9b2d5b64a1c172fc
Luuiiz ツ <97256376+luuiizf@users.noreply.github.com>
2024-09-18 03:01:43 +0000

    Classes e Persistencia

--- persistencia/medicos.py
+++ persistencia/medicos.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from persistenciaBase import PersistenciaBase
-from entidades.medico import Medico
+from entitades.medico import Medico
 import json
 
 
